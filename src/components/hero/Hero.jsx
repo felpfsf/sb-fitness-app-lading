9c54771d377d34cff13610d45e628dd5ae153c23
felpfsf <feferreira.sf@gmail.com>
2022-08-04 20:41:42 -0300

    assets

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='w-full h-screen bg-[url("/assets/hero-img-mobile.png")] md:bg-[url("/assets/hero-img-desktop.png")] bg-right-top bg-no-repeat'>
+      {/* Call to action */}
+      <div className='flex md:px-8 xl:justify-center'>
+        <div className='md:w-[484px] pt-[112px] md:pt-40 px-16 flex flex-col justify-center'>
+          <h3 className='text-sm'>author's slim body program</h3>
+          <h1 className='text-4xl md:text-5xl leading-8 font-bold mt-[18px]'>Get slimmer in <strong className='text-sbPallet-accent'>56</strong> days</h1>
+          <h2 className='text-sm md:text-base leading-relaxed mt-[18px] mb-[18px]'>
+            Our professional fitness trainers and nutritionists will lead you to a <span className='underline'>slim</span>, <span className='underline'>healthy</span> and happy life.
+          </h2>
+          <a className='w-[212px] h-9 p-3 text-lg bg-sbPallet-accent rounded-lg flex items-center justify-center' href="#">Learn more</a>
+        </div>
+        {/* hero image visible on tablet desktop */}
+        <img className='hidden lg:block pt-36' src="/assets/girl-fitness.png" alt="" />
+      </div>
+
+      {/* Cards */}
+      <div className='pt-10 px-12 flex flex-wrap md:justify-center gap-3 md:gap-14'>
+        {/* card #1 */}
+        <div className='w-[117px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
+          <div>
+            <img src="/assets/dumb-bell.png" alt="Dumb-bell Clipart" />
+          </div>
+          <h2 className='text-sm font-bold mt-2'>Fast</h2>
+          <p className='text-xs text-center leading-relaxed mt-2'>First results after the first weeks</p>
+        </div>
+        {/* card #2 */}
+        <div className='w-[117px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
+          <div>
+            <img src="/assets/cherry.png" alt="Cherry Clipart" />
+          </div>
+          <h2 className='text-sm font-bold mt-2'>Tasty</h2>
+          <p className='text-xs text-center leading-relaxed mt-2'>Weight loss without fasting or dieting</p>
+        </div>
+        {/* card #3 */}
+        <div className='w-[117px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
+          <div>
+            <img src="/assets/cam-monitor.png" alt="Camera Clipart" />
+          </div>
+          <h2 className='text-sm font-bold mt-2'>Safely</h2>
+          <p className='text-xs text-center leading-none mt-2'>Under the supervision of experienced professionals</p>
+        </div>
+        {/* card #4 */}
+        <div className='w-[117px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
+          <div>
+            <img src="/assets/scale.png" alt="Scale Clipart" />
+          </div>
+          <h2 className='text-sm font-bold mt-2'>Remotely</h2>
+          <p className='text-xs text-center leading-none mt-2'>Materials and recommendations online</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState()
+
+  const handleOpenMenu = () => { setOpenMenu(!openMenu) }
+
+  return (
+    <header className='w-full z-10 absolute py-6 px-16 lg:px-24 flex items-center justify-between'>
+      <a href="#home">
+        <img className='w-8 h-8' src="/assets/SB-logo.png" alt="SB logo" />
+      </a>
+      {/* Menu Tablet / Desktop */}
+      <nav className='hidden xl:w-full lg:flex justify-center'>
+        <ul className='text-center uppercase flex gap-8'>
+          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#" onClick={() => setOpenMenu(false)}>Home</a></li>
+          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#" onClick={() => setOpenMenu(false)}>Program</a></li>
+          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#" onClick={() => setOpenMenu(false)}>Results</a></li>
+          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#" onClick={() => setOpenMenu(false)}>Contact Us</a></li>
+        </ul>
+      </nav>
+      <a className='hidden lg:block w-40 p-2 text-xs text-center uppercase border rounded-lg hover:bg-sbPallet-accent hover:border-sbPallet-accent ease-in-out duration-200' href="#">Request a call</a>
+      {/* Button and Menu Mobile */}
+      {
+        openMenu ?
+          <AiOutlineClose onClick={handleOpenMenu} className='lg:hidden z-10 text-[32px] text-white cursor-pointer' />
+          :
+          <AiOutlineMenu onClick={handleOpenMenu} className='lg:hidden z-10 text-[32px] text-white cursor-pointer' />
+      }
+      <nav className=
+        {openMenu ?
+          ('fixed top-0 right-0 h-screen w-[70%] text-center bg-sbPallet-cardBg/70 backdrop-blur-[80px] pt-28 ease-in-out duration-300')
+          :
+          ('fixed top-0 right-[-100%] h-screen w-[70%] text-center bg-sbPallet-cardBg/70 backdrop-blur-[80px] pt-28 ease-in-out duration-300')}
+      >
+        <ul className='text-center uppercase'>
+          <li className='text-lg font-bold pb-8'><a className='hover:underline underline-offset-[16px]' href="#" onClick={() => setOpenMenu(false)}>Home</a></li>
+          <li className='text-lg font-bold pb-8'><a className='hover:underline underline-offset-[16px]' href="#" onClick={() => setOpenMenu(false)}>Program</a></li>
+          <li className='text-lg font-bold pb-8'><a className='hover:underline underline-offset-[16px]' href="#" onClick={() => setOpenMenu(false)}>Results</a></li>
+          <li className='text-lg font-bold pb-8'><a className='hover:underline underline-offset-[16px]' href="#" onClick={() => setOpenMenu(false)}>Contact Us</a></li>
+        </ul>
+        <a className='w-40 p-2 text-xs text-center uppercase border rounded-lg hover:bg-sbPallet-accent hover:border-sbPallet-accent ease-in-out duration-200' href="#">Request a call</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
