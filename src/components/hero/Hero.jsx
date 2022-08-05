2e686da08bf6c815848d802bc3946964a254fa8f
felpfsf <feferreira.sf@gmail.com>
2022-08-05 13:08:47 -0300

    changing font styles

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,50 +6,50 @@ const Hero = () => {
       {/* Call to action */}
       <div className='flex md:px-8 xl:justify-center'>
         <div className='md:w-[484px] pt-[112px] md:pt-40 px-16 flex flex-col justify-center'>
-          <h3 className='text-sm'>author's slim body program</h3>
-          <h1 className='text-4xl md:text-5xl leading-8 font-bold mt-[18px]'>Get slimmer in <strong className='text-sbPallet-accent'>56</strong> days</h1>
-          <h2 className='text-sm md:text-base leading-relaxed mt-[18px] mb-[18px]'>
+          <h3 className='text-sm font-light'>author's slim body program</h3>
+          <h1 className='text-4xl md:text-5xl leading-8 font-black mt-[18px]'>Get slimmer in <strong className='text-sbPallet-accent'>56</strong> days</h1>
+          <h2 className='text-sm font-light md:text-base leading-relaxed mt-[18px] mb-[18px]'>
             Our professional fitness trainers and nutritionists will lead you to a <span className='underline'>slim</span>, <span className='underline'>healthy</span> and happy life.
           </h2>
-          <a className='w-[212px] h-9 p-3 text-lg bg-sbPallet-accent rounded-lg flex items-center justify-center' href="#">Learn more</a>
+          <a className='w-[212px] h-9 p-3 text-lg font-medium bg-sbPallet-accent rounded-lg flex items-center justify-center' href="#">Learn more</a>
         </div>
         {/* hero image visible on tablet desktop */}
         <img className='hidden lg:block pt-36' src="/assets/girl-fitness.png" alt="Girl lifting in smith machine" />
       </div>
 
       {/* Cards */}
-      <div className='pt-10 px-12 flex flex-wrap md:justify-center gap-3 md:gap-14'>
+      <div className='pt-10 px-10 flex flex-wrap justify-center gap-3 md:gap-14'>
         {/* card #1 */}
-        <div className='w-[117px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
+        <div className='w-[134px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
           <div>
             <img src="/assets/dumb-bell.png" alt="Dumb-bell Clipart" />
           </div>
           <h2 className='text-sm font-bold mt-2'>Fast</h2>
-          <p className='text-xs text-center leading-relaxed mt-2'>First results after the first weeks</p>
+          <p className='text-xs font-light text-center leading-relaxed mt-2'>First results after the first weeks</p>
         </div>
         {/* card #2 */}
-        <div className='w-[117px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
+        <div className='w-[134px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
           <div>
             <img src="/assets/cherry.png" alt="Cherry Clipart" />
           </div>
           <h2 className='text-sm font-bold mt-2'>Tasty</h2>
-          <p className='text-xs text-center leading-relaxed mt-2'>Weight loss without fasting or dieting</p>
+          <p className='text-xs font-light text-center leading-relaxed mt-2'>Weight loss without fasting or dieting</p>
         </div>
         {/* card #3 */}
-        <div className='w-[117px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
+        <div className='w-[134px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
           <div>
             <img src="/assets/cam-monitor.png" alt="Camera Clipart" />
           </div>
           <h2 className='text-sm font-bold mt-2'>Safely</h2>
-          <p className='text-xs text-center leading-none mt-2'>Under the supervision of experienced professionals</p>
+          <p className='text-xs font-light text-center leading-none mt-2'>Under the supervision of experienced professionals</p>
         </div>
         {/* card #4 */}
-        <div className='w-[117px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
+        <div className='w-[134px] sm:w-36 md:w-56 h-36 md:h-44 p-1 rounded-xl bg-sbPallet-cardBg flex flex-col items-center justify-center'>
           <div>
             <img src="/assets/scale.png" alt="Scale Clipart" />
           </div>
           <h2 className='text-sm font-bold mt-2'>Remotely</h2>
-          <p className='text-xs text-center leading-none mt-2'>Materials and recommendations online</p>
+          <p className='text-xs font-light text-center leading-none mt-2'>Materials and recommendations online</p>
         </div>
       </div>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,12 +15,12 @@ const Navbar = () => {
       <nav className='hidden xl:w-full lg:flex justify-center'>
         <ul className='text-center uppercase flex gap-8'>
           <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#" onClick={() => setOpenMenu(false)}>Home</a></li>
-          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#" onClick={() => setOpenMenu(false)}>Program</a></li>
+          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#about" onClick={() => setOpenMenu(false)}>Program</a></li>
           <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#" onClick={() => setOpenMenu(false)}>Results</a></li>
           <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#" onClick={() => setOpenMenu(false)}>Contact Us</a></li>
         </ul>
       </nav>
-      <a className='hidden lg:block w-40 p-2 text-xs text-center uppercase border rounded-lg hover:bg-sbPallet-accent hover:border-sbPallet-accent ease-in-out duration-200' href="#">Request a call</a>
+      <a className='hidden lg:block w-40 p-2 text-xs text-center uppercase border rounded-lg hover:font-bold hover:bg-sbPallet-accent hover:border-sbPallet-accent ease-in-out duration-200' href="#">Request a call</a>
 
       {/* Button and Menu Mobile */}
       
@@ -38,7 +38,7 @@ const Navbar = () => {
       >
         <ul className='text-center uppercase'>
           <li className='text-lg font-bold pb-8'><a className='hover:underline underline-offset-[16px]' href="#" onClick={() => setOpenMenu(false)}>Home</a></li>
-          <li className='text-lg font-bold pb-8'><a className='hover:underline underline-offset-[16px]' href="#" onClick={() => setOpenMenu(false)}>Program</a></li>
+          <li className='text-lg font-bold pb-8'><a className='hover:underline underline-offset-[16px]' href="#about" onClick={() => setOpenMenu(false)}>Program</a></li>
           <li className='text-lg font-bold pb-8'><a className='hover:underline underline-offset-[16px]' href="#" onClick={() => setOpenMenu(false)}>Results</a></li>
           <li className='text-lg font-bold pb-8'><a className='hover:underline underline-offset-[16px]' href="#" onClick={() => setOpenMenu(false)}>Contact Us</a></li>
         </ul>
