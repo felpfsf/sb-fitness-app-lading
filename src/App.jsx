a711ac3f9877aa35499ae17b66dc72ab880a170d
felpfsf <feferreira.sf@gmail.com>
2022-08-06 11:55:14 -0300

    testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/about/About"
 import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 import Results from "./components/results/Results"
+import Testimonials from "./components/testimonials/Testimonials"
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Results />
+      <Testimonials />
     </>
   )
 }
