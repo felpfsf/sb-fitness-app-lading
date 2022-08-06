6692a0561bf4116245fd84ded35235e0014842af
felpfsf <feferreira.sf@gmail.com>
2022-08-06 19:29:29 -0300

    footer section ready

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,20 @@
 const Footer = () => {
-  return(
-    <div>
-      Footer
-    </div>
+  return (
+    <section className="w-full mt-16 py-4 lg:py-10 bg-sbPallet-cardBg flex items-center justify-around">
+      <a href="#home">
+        <img className='w-8 h-8' src="/assets/SB-logo.png" alt="SB logo" />
+      </a>
+      {/* Footer Nav */}
+      <nav className='flex flex-col lg:flex-row items-center gap-4 lg:gap-40'>
+        <ul className='text-center uppercase flex flex-col lg:flex-row gap-4'>
+          <li className='text-base font-light md:pb-0'><a className='hover:text-sbPallet-hover hover:underline underline-offset-8' href="#">Home</a></li>
+          <li className='text-base font-light md:pb-0'><a className='hover:text-sbPallet-hover hover:underline underline-offset-8' href="#about">Program</a></li>
+          <li className='text-base font-light md:pb-0'><a className='hover:text-sbPallet-hover hover:underline underline-offset-8' href="#results">Results</a></li>
+          <li className='text-base font-light md:pb-0'><a className='hover:text-sbPallet-hover hover:underline underline-offset-8' href="#contact">Contact Us</a></li>
+        </ul>
+        <a className='hidden lg:block w-40 p-2 text-xs text-center uppercase border rounded-lg hover:font-bold hover:bg-sbPallet-accent hover:border-sbPallet-accent ease-in-out duration-200' href="#">Request a call</a>
+      </nav>
+    </section>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,16 +14,16 @@ const Navbar = () => {
       {/* Menu Tablet / Desktop */}
       <nav className='hidden xl:w-full lg:flex justify-center'>
         <ul className='text-center uppercase flex gap-8'>
-          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#" onClick={() => setOpenMenu(false)}>Home</a></li>
-          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#about" onClick={() => setOpenMenu(false)}>Program</a></li>
-          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#results" onClick={() => setOpenMenu(false)}>Results</a></li>
-          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#contact" onClick={() => setOpenMenu(false)}>Contact Us</a></li>
+          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#">Home</a></li>
+          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#about">Program</a></li>
+          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#results">Results</a></li>
+          <li className='text-base font-bold pb-8 md:pb-0'><a className='hover:underline underline-offset-8' href="#contact">Contact Us</a></li>
         </ul>
       </nav>
       <a className='hidden lg:block w-40 p-2 text-xs text-center uppercase border rounded-lg hover:font-bold hover:bg-sbPallet-accent hover:border-sbPallet-accent ease-in-out duration-200' href="#">Request a call</a>
 
       {/* Button and Menu Mobile */}
-      
+
       {
         openMenu ?
           <AiOutlineClose onClick={handleOpenMenu} className='lg:hidden z-10 text-[32px] text-white cursor-pointer' />
