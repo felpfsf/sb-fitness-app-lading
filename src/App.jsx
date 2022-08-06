fd9caf52ad8a030f1e6eebc09793ab6117df7a06
felpfsf <feferreira.sf@gmail.com>
2022-08-06 19:16:39 -0300

    footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./components/about/About"
 import Contact from "./components/contact/Contact"
+import Footer from "./components/footer/Footer"
 import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 import Results from "./components/results/Results"
@@ -16,6 +17,7 @@ function App() {
       <Results />
       <Testimonials />
       <Contact />
+      <Footer />
     </>
   )
 }
